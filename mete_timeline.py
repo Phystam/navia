fd95da949dd422fa3769f31229ef487ce1cb7320
phystam <phystam.tc@gmail.com>
2025-08-24 00:02:57 +0900

    不要ファイルを削除

--- mete_timeline.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtCore import QObject, Signal, Slot
-
-class MeteTimeline(QObject):
-    def __init__(self,parent=None):
-        super().__init__(parent=None)
-        
-    def _load_data(self):
-        pass
